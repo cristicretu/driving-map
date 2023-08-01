2c83ea80435cfaf7052eca00d8814f3712d80630
Cristian Crețu <crisemcr@gmail.com>
2023-08-01 22:03:45 +0300

    save

--- next.config.js
+++ next.config.js
@@ -6,6 +6,14 @@ const nextConfig = {
     NEXT_PUBLIC_MAPBOX_GL_ACCESS_TOKEN:
       process.env.NEXT_PUBLIC_MAPBOX_GL_ACCESS_TOKEN,
   },
+  webpack: (config, { isServer }) => {
+    // Fixes npm packages that depend on `fs` module
+    if (!isServer) {
+      config.resolve.fallback = { fs: false }
+    }
+
+    return config
+  },
 }
 
 module.exports = nextConfig
